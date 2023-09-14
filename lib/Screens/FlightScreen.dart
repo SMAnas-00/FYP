5541c86159abe53958b3193c744c055409ff4163
SMAnas-00 <gouheranas4456@gmail.com>
2023-09-15 00:56:47 +0500

    Add data into flight screen

--- lib/Screens/FlightScreen.dart
+++ lib/Screens/FlightScreen.dart
@@ -231,57 +231,147 @@ class _FlightListState extends State<FlightList> {
             if (snapshot.connectionState == ConnectionState.active) {
               QuerySnapshot querySnapshot = snapshot.data;
               List<QueryDocumentSnapshot> listqurey = querySnapshot.docs;
-              return Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 20,
+              return ListView.builder(
+                itemCount: listqurey.length,
+                itemBuilder: (context, index) {
+                  QueryDocumentSnapshot document = listqurey[index];
+                  final img = document['flight_imageURL'];
+                  return Card(
+                    margin: const EdgeInsets.all(4),
+                    color: const Color(0xffffffff),
+                    shadowColor: const Color(0xff000000),
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
                     ),
-                    ListView.builder(
-                        physics: const ClampingScrollPhysics(),
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        itemCount: listqurey.length,
-                        itemBuilder: (context, index) {
-                          QueryDocumentSnapshot document = listqurey[index];
-                          return GestureDetector(
-                            child: FlightCard(
-                              disc_price: document['discount_ticket_price'],
-                              flight_day: document['flight_day'],
-                              flight_name: document['airline_name'],
-                              flight_time: document['flight_time'],
-                              price: document['ticket_price'],
-                              departure_city: document['departure_city'],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(12.0),
+                                child:
+
+                                    ///***If you have exported images you must have to copy those images in assets/images directory.
+                                    Image(
+                                  image: NetworkImage("${img[0]}"),
+                                  height: 140,
+                                  width: MediaQuery.of(context).size.width,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                                child: Text(
+                                  document['airline_name'],
+                                  textAlign: TextAlign.start,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.clip,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 16,
+                                    color: Color(0xff000000),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 4, horizontal: 0),
+                                child: Text(
+                                  '${document['departure']}',
+                                  textAlign: TextAlign.start,
+                                  overflow: TextOverflow.clip,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 12,
+                                    color: Color(0xff000000),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 4, horizontal: 0),
+                                child: Text(
+                                  '${document['destination']}',
+                                  textAlign: TextAlign.start,
+                                  overflow: TextOverflow.clip,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 12,
+                                    color: Color(0xff000000),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            alignment: Alignment.center,
+                            margin: const EdgeInsets.all(0),
+                            padding: const EdgeInsets.all(0),
+                            width: MediaQuery.of(context).size.width,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: const Color(0xff3a57e8),
+                              shape: BoxShape.rectangle,
+                              borderRadius: const BorderRadius.only(
+                                  bottomLeft: Radius.circular(12.0),
+                                  bottomRight: Radius.circular(12.0)),
+                              border: Border.all(
+                                  color: const Color(0x4d9e9e9e), width: 1),
                             ),
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => FlightDetails(
-                                            flight_name:
-                                                document['airline_name'],
-                                            FlightImageURL:
-                                                document['flight_imageURL'],
-                                            FlightPrice: document[
-                                                'discount_ticket_price'],
-                                            departure: document['departure'],
-                                            departure_city:
-                                                document['departure_city'],
-                                            destination:
-                                                document['destination'],
-                                            destination_city:
-                                                document['destination_city'],
-                                            flight_time:
-                                                document['flight_time'],
-                                            flight_id: document['flight_id'],
-                                          )));
-                            },
-                          );
-                        })
-                  ],
-                ),
+                            child: const Text(
+                              "View More",
+                              textAlign: TextAlign.start,
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 16,
+                                color: Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => FlightDetails(
+                                          FlightImageURL:
+                                              document['flight_imageURL'],
+                                          FlightPriceEco:
+                                              document['economy_price'],
+                                          FlightPriceBus:
+                                              document['business_price'],
+                                          departure: document['departure'],
+                                          destination: document['destination'],
+                                          departure_time:
+                                              document['departure_time'],
+                                          destination_time:
+                                              document['destination_time'],
+                                          flight_id: document['flight_id'],
+                                          flight_name: document['airline_name'],
+                                          latitude: document['departure_lat'],
+                                          longitude: document['departure_lng'],
+                                        )));
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
               );
             }
             return const CircularProgressIndicator();
